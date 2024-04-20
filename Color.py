03c573923759f2eef328c7697bc90024b1b70730
Sylverstone <sylvio0801@gmail.com>
2024-04-20 18:24:00 -0400

    Premier commit, bases du projet intégré

---
+++ Color.py
@@ -0,0 +1,12 @@
+class Color:
+    """
+        Class repertoriant toute les couleurs possible dans l'applications
+    """
+    def __init__(self):        
+        self.Gris = (64,64,64)
+        self.Noir = (33,33,33)
+        self.Bleu = (106,109,255)
+        self.Noir_clair = (40,40,40)
+        self.Gris_clair = (142,142,142)
+        self.Bleu_clair = (158,155,255)
+        
---
+++ font_import.py
@@ -0,0 +1,10 @@
+import os
+
+csm = "Comic Sans Ms"
+arial = "Arial"
+chivo_titre = os.path.join("font","chivo","Chivo-Bold.otf")
+dream_orphans = os.path.join("font","dream_orphans","Dream Orphans.otf") 
+apple_titre = os.path.join("font","apple_garamond","AppleGaramond-Light.ttf") 
+beackman = os.path.join("font","Beckman")
+TNN = os.path.join("font","TNN.ttf") 
+pw_christmas_candies = os.path.join("font","pwchristmascandies","PWChristmascandies.ttf")
